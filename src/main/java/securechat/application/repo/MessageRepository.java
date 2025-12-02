992a734b0c6336ce764744e37da89f089887496a
varshith reddy <varshithreddy1009@gmail.com>
2025-12-02 19:53:02 +0530

    first commit

--- src/main/java/securechat/application/repo/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package securechat.application.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import securechat.application.entity.Message;
-
-public interface MessageRepository extends JpaRepository<Message, Long>
-{
-
-}
